b27f843e1aed723567ea68960907190bfaa5663f
Nordine000 <Ounissi970@gmail.com>
2024-10-21 13:45:42 +0000

    C

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+gcc 6 - size.c - m32 - o size32 2 > / tmp / 32;
+gcc 6 - size.c - m64 - o size64 2 > / tmp / 64;
+return (0);
+}
