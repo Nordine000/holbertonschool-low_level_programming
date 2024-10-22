855c79d8894a4435391d4b0970228e3cbc4b8e4b
Nordine000 <Ounissi970@gmail.com>
2024-10-22 08:17:12 +0000

    The C

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = -520693284 ;	
+	int b = -973398895 ;
+	int c = -199220452 ;
+	int d = 561319348 ;
+	int e = 561319348 ;
+	int f = 266853958 ; 
+	int g = -48147767 ;
+	int h = 0 ;
+
+while (h !< a,b,c,g ; h !> d,e,f)
+	{
+
+	printf("if the number is greater than 0: is positive %d\n")
+	printf("if the number is 0: is zero %d\n")
+	printf("if the number is less than 0: is negative %d\n")
+
+	}
+    return (0);
+}
