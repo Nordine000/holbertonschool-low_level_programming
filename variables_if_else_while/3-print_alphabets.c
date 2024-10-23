fbf289e7eb2d7cb2d6a520708d812e692fe015ff
Nordine000 <ounissi970@gmail.com>
2024-10-23 13:08:03 +0000

    The C

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+char u;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+int main(void)
+{
+for (u = 'A'; u <= 'Z'; u++)
+{
+putchar(u);
+}
+putchar('\n');
+return (0);
+}
+}
