5032db94cba09be683079f6afab1f81ac349c8ec
Nordine000 <Ounissi970@gmail.com>
2024-10-24 10:50:59 +0000

    The C

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -5,3 +5,19 @@
  *
  * Return: Always 0 (Success)
  */
+
+int main(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
