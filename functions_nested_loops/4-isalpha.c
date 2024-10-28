440af5f51e56ab60726ebd68dd252964f87d1b08
Nordine000 <ounissi970@gmail.com>
2024-10-28 09:02:10 +0000

    The C

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isalpha- check the code.
+*
+* Return: Always 0.
+*/
+
+int _isalpha(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
