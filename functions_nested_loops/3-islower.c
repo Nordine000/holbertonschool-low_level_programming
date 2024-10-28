47233a1b552412186c51e178b959a0bbf59117f8
Nordine000 <ounissi970@gmail.com>
2024-10-28 08:56:55 +0000

    The C

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * _islower- check the code.
@@ -9,24 +8,12 @@
 
 int _islower(int c)
 {
-return (c >= 'a' && c <= 'z') ? 1 : 0;
-}
-void _putchar(char c)
+if (c >= 'a' && c <= 'z')
 {
-putchar(c);
+return (1);
 }
-int main(void) {
-char b[] = {'a', 'b', 'C', 'D', 'E', 'F', 'J', 'H', 'I', 'j', 'K', 'l', 'M', 'n', 'o', 'p', 'Q', 'R', 's', 't', 'U', 'v', 'W', '\0'};
-for (int i = 0; b[i] != '\0'; i++)
+else
 {
-int r = _islower(b[i]);
-if (r)
-{
-_putchar('1');
-} else {
-_putchar('0');
+return (0);
 }
-}
-_putchar('\n');
-return 0;
 }
