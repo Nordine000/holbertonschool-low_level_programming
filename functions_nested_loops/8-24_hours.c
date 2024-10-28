309cd3441ea60f26cd1520ec864a9a5fcfb03830
Nordine000 <ounissi970@gmail.com>
2024-10-28 15:36:01 +0000

    The C

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ for (heure = 0; heure < 24; heure++)
 {
 for (minute = 0; minute < 60; minute++)
 {
-_putchar((minute / 10) + '0');
-_putchar((minute % 10) + '0');
-_putchar(':');
 _putchar((heure / 10) + '0');
 _putchar((heure % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
 _putchar('\n');
 }
 }
