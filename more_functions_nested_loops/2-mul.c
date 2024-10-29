c01b830c7d5cfb69164a5e06cfb039606314c8ed
Nordine000 <ounissi970@gmail.com>
2024-10-29 14:50:44 +0000

    The C

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul- check the code
+ * @a: on lui a assigner une valeur
+ * @b: on lui a assigner une valeur
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
