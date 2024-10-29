9440b40303ef00e98a78521347fb6cd52adce7f0
Nordine000 <ounissi970@gmail.com>
2024-10-29 15:04:05 +0000

    The C

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers- check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int a;
+for (a = 0; a <= 10; a++)
+{
+	putchar(a + '0');
+}
+	putchar('\n');
+}
