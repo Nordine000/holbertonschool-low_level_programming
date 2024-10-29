8ab06b8f784d88e7c7e6c7c051e5a51d729d6693
Nordine000 <ounissi970@gmail.com>
2024-10-29 18:46:16 +0000

    The C

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,7 @@ void more_numbers(void)
 int a;
 int b;
 
-for (a = 0; a <= 10; a++)
-{
-for (b = 0; b <= 14; b++)
+for (a = 0, b = 0; a <= 10 && b <= 14; a++, b++)
 {
 if (b > 9)
 {
@@ -22,5 +20,4 @@ _putchar('1');
 _putchar((b % 10) + '0');
 }
 _putchar('\n');
-}
 }
