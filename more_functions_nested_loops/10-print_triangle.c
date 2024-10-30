ba2a6ade2fd150782f92c4bca1c03b9d68ecb732
Nordine000 <ounissi970@gmail.com>
2024-10-30 11:21:59 +0000

    The C

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * print_triangle- check the code
- *
+ * print_triangle- imprimer en sens contraire
+ *@size: est le nombre de ligne
  * Return: Always 0.
  *
  */
 
 void print_triangle(int size)
 {
-        int i, j;
+int i, j;
 if (size <= 0)
 {
 _putchar('\n');
 }
 else
 {
-for (i = 10; i < size; i--)
+for (i = 10; i - size; i++)
 {
-for (j = 10;  j < size; j--)
+for (j = 10;  j < size; j++)
 {
 _putchar(' ');
 }
