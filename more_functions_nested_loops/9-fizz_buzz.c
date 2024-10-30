ec69e61271b16c47a53fe47b17ebeb7de5d28522
Nordine000 <ounissi970@gmail.com>
2024-10-30 09:43:36 +0000

    The C

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,10 +11,7 @@ int main(void)
 int a;
 for (a = 1; a <= 100; a++)
 {
-if (a % 3 == 0 && a % 5 == 0)
-{
-printf("FizzBuzz");
-}
+	printf("%d", a);
 if (a % 3 == 0)
 {
 printf("Fizz");
@@ -22,6 +19,10 @@ printf("Fizz");
 if (a % 5 == 0)
 {
 printf("Buzz");
+}
+else if (a % 3 == 0 && a % 5 == 0)
+{
+printf("FizzBuzz");
 }
 }
 return (0);
