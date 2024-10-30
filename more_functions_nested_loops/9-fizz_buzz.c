be34f68f07d2a186ba0a253754989ad09d92aed2
Nordine000 <ounissi970@gmail.com>
2024-10-30 09:38:15 +0000

    The C

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,28 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - m'imprime les nombres de 1 a 100 et remplace 3 et 5
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 int a;
-
-for (a >= 0; a <= 100, a++)
+for (a = 1; a <= 100; a++)
 {
 if (a % 3 == 0 && a % 5 == 0)
 {
+printf("FizzBuzz");
+}
 else if (a % 3 == 0)
 {
-putchar("Fizz");
+printf("Fizz");
 }
 else if (a % 5 == 0)
 {
-putchar("Buzz");
+printf("Buzz");
 }
 }
-}
-putchar('\n');
+return (0);
 }
