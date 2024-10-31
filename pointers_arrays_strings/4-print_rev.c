ecfb1cb8f1dc6005d777032e9044dc3eb7d4a989
Nordine000 <ounissi970@gmail.com>
2024-10-31 12:36:47 +0000

    The C

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev- inverse ma chaine de caractere
+ *@s: contient ma chaine
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+
+	int end;
+	int start;
+	char c;
+while (s[end] != '\0')
+	{
+	_putchar(*s);
+	end++;
+	}
+while (start < end)
+{
+	c = s[start];
+	s[start] = s[end];
+	s[end] = c;
+
+	start++;
+	end--;
+
+}
+_putchar('\n');
+}
