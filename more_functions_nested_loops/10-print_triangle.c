d142227375c3b5a6b169b30dbc5c0dede8a4b1c9
Nordine000 <ounissi970@gmail.com>
2024-10-31 23:38:14 +0000

    The C

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -14,15 +14,16 @@ if (size <= 0)
 {
 _putchar('\n');
 }
-else
+for (i = 1; i <= size; i++)
 {
-for (i = 10; i - size; i++)
-{
-for (j = 10;  j < size; j++)
+for (j = 0;  j < size - i; j++)
 {
 _putchar(' ');
 }
+for (j = 0; j < i; j++)
+{
 _putchar('#');
 }
 }
+_putchar('\n');
 }
