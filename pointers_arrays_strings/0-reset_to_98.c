d35c0a9a8a786efe3eae88c4e41881874199bc3b
Nordine000 <ounissi970@gmail.com>
2024-10-31 09:07:09 +0000

    The c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reset_to_98- donne une valeur a *n
+ *
+ * @n: est le pointeur
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+
+}
