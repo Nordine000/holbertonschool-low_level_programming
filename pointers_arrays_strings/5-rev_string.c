0f17c2d9c999704a708775650656ab71d2357008
Nordine000 <ounissi970@gmail.com>
2024-11-01 14:15:13 +0000

    The C

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string- check the code
+ *@s: est un pointeur qui pointe vers une chaine de caractere
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int end = 0;
+
+	int start = 0;
+
+	while (*s != '\0')
+	{
+		end++;
+	}
+	while (start < end)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+
+		start++;
+		end--;
+	}
+}
