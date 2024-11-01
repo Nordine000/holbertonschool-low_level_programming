beb10b9f93fc26311f56115e12415671508c0fc1
Nordine000 <ounissi970@gmail.com>
2024-11-01 15:25:15 +0000

    The C

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,12 +2,33 @@
 
 /**
  * puts_half- check the code
- *
+ *@str: pointeur
  * Return: Always 0.
  */
 void puts_half(char *str)
 {
+	int i = 0;
+	int j = 0;
 
+	while (str[i] != '\0')
+	{
+		i++;
+		j++;
+	}
 
-
+	if (j % 2 == 0)
+	{
+		i = j / 2;
+}
+	else
+{
+	(i = (j + 1) / 2);
+}
+		while
+			(str[i] != '\0')
+			{
+				_putchar(str[i]);
+				i++;
+			}
+	_putchar('\n');
 }
