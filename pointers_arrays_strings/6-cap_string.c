e5139f1b565d954d2530652cd3fb811fe64243e2
Nordine000 <ounissi970@gmail.com>
2024-11-04 17:46:50 +0000

    The C

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,15 +9,31 @@
 
 char *cap_string(char *n)
 {
-	int i = 0;
+	int index = 0;
 
-	while (n[i] != '\0')
+	while (n[index])
 	{
-	if (n[i] >= 'a' && n[i] <= 'z')
-		{
-			n[i] = n[i] - 32;
-		}
-	i++;
+		while (!(n[index] >= 'a' && n[index] <= 'z'))
+			index++;
+
+		if (n[index - 1] == ' ' ||
+				n[index - 1] == '\t' ||
+				n[index - 1] == '\n' ||
+				n[index - 1] == ',' ||
+				n[index - 1] == ';' ||
+				n[index - 1] == '.' ||
+				n[index - 1] == '!' ||
+				n[index - 1] == '?' ||
+				n[index - 1] == '"' ||
+				n[index - 1] == '(' ||
+				n[index - 1] == ')' ||
+				n[index - 1] == '{' ||
+				n[index - 1] == '}' ||
+				index == 0)
+			n[index] -= 32;
+
+		index++;
 	}
+
 	return (n);
 }
