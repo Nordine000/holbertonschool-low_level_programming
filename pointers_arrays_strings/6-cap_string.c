ef3d503f4f17338c3cefb736dd40a9c6b2e65bb6
Nordine000 <ounissi970@gmail.com>
2024-11-04 14:01:56 +0000

    The C

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ **cap_string- check the code
+ *@n: liste de ma chaine
+ * Return: Always 0.
+ */
+
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+	i++;
+	}
+	return (n);
+}
+
