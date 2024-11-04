ffe4fbebccd3dfb252f2e8927e23412d26e2e1c0
Nordine000 <ounissi970@gmail.com>
2024-11-04 10:57:03 +0000

    The C

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncpy- check the code
+ *@dest: liste
+ *@src: liste
+ *@n : incrementation
+ *
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+while (i < n)
+{
+
+dest[i] = '\0';
+i++;
+}
+return (0);
+}
