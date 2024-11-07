314bb3970981c88f92b8cc74ace865655c41dd2d
Nordine000 <ounissi970@gmail.com>
2024-11-07 11:13:03 +0000

    The C

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard- echec et mat
+ *@a:tableau
+ * Return: Always 0.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int o, p;
+
+	for (o = 0; o <= 8; o++)
+	{
+		for (p = 0; p <= 8; p++)
+		{
+			_putchar(a[o][p]);
+		}
+	}
+}
