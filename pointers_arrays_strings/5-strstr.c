96f4223764c25070c26b536a7aa9459d20bb9ebc
Nordine000 <ounissi970@gmail.com>
2024-11-07 10:00:53 +0000

    The C

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,20 +2,44 @@
 #include <stdio.h>
 
 /**
- * *_strstr- check the code
+ * *_strstr- utilisée pour la correspondance de chaîn
+ *@haystack: chaîne principal a examine
+ *@needle: sous-chaînerechercher dans la cha�ne
  *
  * Return: Always 0.
  */
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i, j, o = 0, m = 0;
+	int c = 0;
 
+	while (needle[o] != '\0')
+	{
+		c++;
+		o++;
+	}
 
+	for (i = 0; haystack[i] != '\0' ; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				m++;
+				i++;
+			}
+		}
 
-
-
-
-
-
-
+		if (c == m)
+		{
+			haystack = haystack + (i - c);
+			return (haystack);
+		}
+		else
+		{
+			m = 0;
+		}
+	}
+	return ('\0');
 }
