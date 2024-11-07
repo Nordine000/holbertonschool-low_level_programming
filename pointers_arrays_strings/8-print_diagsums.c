5747ea5fa40b909ee90641423557bd105c29c82d
Nordine000 <ounissi970@gmail.com>
2024-11-07 16:19:39 +0000

    The C

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,22 +10,16 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 	int o = 0, p = 0;
 
-	for (i = 0 ; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				o += *(a + (i * size) + j);
-			}
-			if (i + j == size - 1)
-			{
-				p += *(a + (i * size) + j);
-			}
-		}
-		printf("%d, %d \n", o, p);
+		o += a[i * size + i];
 	}
+	for (i = 0; i < size; i++)
+	{
+		p += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", o, p);
 }
