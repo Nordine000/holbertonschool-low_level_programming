fb6a1ea9645ba33d1f9b95d93f8bcceeeced4250
Nordine000 <Ounissi970@gmail.com>
2024-11-08 18:17:04 +0000

    The C

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial- renvoie un nombre donn√
+ * @n: nombre
+ *
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
