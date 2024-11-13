f1df564e3169315b45e0bd6c63e53a023fe04be5
Nordine000 <ounissi970@gmail.com>
2024-11-13 08:42:40 +0000

    The C

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *str_concat- concatene deux chaines
+ *@s1: chaine 1
+ *@s2: chaine 2
+ * Return: Always 0.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, p, k, j;
+	char *a;
+
+
+	if (s1 == 0)
+	{
+		s1 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+
+		if (s2 == 0)
+		{
+			s2 = "";
+		}
+	for (p = 0; s2[p] != '\0'; p++)
+
+		a = malloc(sizeof(char) * (i + p + 1));
+
+	if (a == 0)
+	{
+		return (0);
+	}
+
+	for (k = 0; k < i; k++)
+	{
+		a[k] = s1[k];
+	}
+	for (j = 0; j <= p; k++, j++)
+	{
+		a[k] = s2[j];
+	}
+
+	return (0);
+}
