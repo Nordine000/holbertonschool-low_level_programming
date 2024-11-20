907b492534c55dda52c3a0096a31bdf1d66397d4
Nordine000 <ounissi970@gmail.com>
2024-11-20 22:25:55 +0000

    The C

---
+++ function_pointers/3-main.c
@@ -0,0 +1,23 @@
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+
+
+op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
