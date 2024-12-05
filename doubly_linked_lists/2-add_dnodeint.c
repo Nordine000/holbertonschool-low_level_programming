df48aa50323a7845ad39d2fd7fc8010e224c86d0
Nordine000 <ounissi970@gmail.com>
2024-12-05 09:33:55 +0000

    The C

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -26,11 +26,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	new->next = *head;
 
-	if (head != NULL)
+	if (*head != NULL)
 	{
-		return (NULL);
-	}
 	(*head)->prev = new;
+	}
 
 	*head = new;
 	return (new);
