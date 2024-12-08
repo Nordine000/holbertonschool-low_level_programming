0542c2c4a1fbc7ff36e5e4cd2a1c87ae491fe3b2
Nordine000 <ounissi970@gmail.com>
2024-12-08 21:04:28 +0000

    The C

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -6,38 +6,41 @@
 
 
 /**
-* add_dnodeint_end - Ajoute un nouveau noeud a la fin
-* @head: Pointeur vers le pointeur
-*@n: valeur inserer
-*
-*
-*
-*
-* Return: Adresse ou NULL
-*/
+ * add_dnodeint_end - Ajoute un nouveau noeud a la fin
+ * @head: Pointeur vers le pointeur
+ *@n: valeur inserer
+ *
+ *
+ *
+ *
+ * Return: Adresse ou NULL
+ */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *last;
+	dlistint_t *new_node;
+	dlistint_t *position;
 
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (*head == NULL)
-		*head = new;
-
-	else
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
+		new_node->prev = NULL;
+		*head = new_node;
+		return (*head);
 	}
 
-	return (*head);
+	position = *head;
+	while (position != NULL && position->next != NULL)
+		position = position->next;
+
+	position->next = new_node;
+	new_node->prev = position;
+
+	return (new_node);
 }
